d861f8ab6c9e668c81f7c8d74df5285664246400
Iaio99 <glnvlln0@gmail.com>
2023-09-24 15:47:25 +0200

    err_code definito come uint8_t e include di stdint.h

--- src/model/error.h
+++ src/model/error.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 #define UNDEF 0
 #define NOT_FOUND 1
 #define NO_SPACE_LEFT 2
 #define EXISTS 3
 
 typedef struct error {
-        opcode_t err_code;
+        uint8_t err_code;
         char* msg;
 }error_t;
 
