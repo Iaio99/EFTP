f3328616f3bbf406dd29dbc7081cdcab5e217ac9
Iaio99 <glnvlln0@gmail.com>
2023-10-06 14:16:17 +0200

    Creata la funzione display_usage

--- src/server.c
+++ src/server.c
@@ -10,33 +10,39 @@
 
 uint16_t port = 8069;
 
+static void display_usage(char **argv) {
+        printf("%s [OPTIONS]\n\n", argv[0]);
+	printf("\nADDITIONAL OPTIONS:\n");
+        printf("\t-p <port>, --port=<port>: Listening port. If not set, default to %d\n", port);
+	printf("\t-r <path>, --root=<path>: Root of the server. If not set, default to ./\n");
+	printf("\t-h, Print this message\n");
+}
 
 static bool parse_cmd_line(int argc, char **argv) {
 	int c;
-//	int option_index;
 
-	while ((c = getopt_long(argc, argv, "r:p:h", long_options, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "hr:p", long_options, NULL)) != -1) {
 		switch (c) {
-			case 'h':
-				puts("Help\n");
-				break;
+                        case 'h':
+                                display_usage(argv);
+                                return false;
+                                break;
 			case 'r':
 				printf("%s\n", optarg);
 				break;
 			case 'p':
-				printf("%s\n", optarg);
 				if (validate_port(optarg)) {
 					port = (uint16_t)strtoul(optarg, NULL, 10);
 				} else {
 					perror("Port is not valid\n");
 					return false;
 				}
-				break;
-
+			        break;
 			case 0:
 			case '?':
 			default:
-                                break;
+                                perror("Invalid options\n");
+                                return false;
 		}
 	}
 
@@ -46,10 +52,10 @@ static bool parse_cmd_line(int argc, char **argv) {
 int main(int argc, char **argv)
 {
 	if(!init_validation())
-                exit(EXIT_FAILURE);
+                return -1;
 
 	if(!parse_cmd_line(argc, argv)) {
-		exit(EXIT_FAILURE);
+		return -1;
 	}
 
         int server = init_server(port);
